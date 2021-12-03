848f748e87c884462e5d55d8891ad2b1ed9041ce
Nate Kidwell <nate@ludicast.com>
2021-12-03 05:03:57 -0500

    update

--- main.rs
+++ main.rs
@@ -1,72 +1,70 @@
 mod util;
-use util::{get_nums, get_directions};
+use util::{get_directions, get_nums};
 
 // Revision to be less iterative
 fn count_increases(nums: &Vec<i64>) -> usize {
-  let range = 0..nums.len() - 1;
-  // is this really needed - wtf
-  let range_array = range.collect::<Vec<usize>>();
-  range_array.iter().filter(
-    |i|
-    nums[**i] < nums[**i + 1] // wtf - how do you rust
-    
-  ).count()
+    let range = 0..nums.len() - 1;
+    // is this really needed - wtf
+    let range_array = range.collect::<Vec<usize>>();
+    range_array
+        .iter()
+        .filter(
+            |i| nums[**i] < nums[**i + 1], // wtf - how do you rust
+        )
+        .count()
 }
 
 // Revision to be less iterative and more dry
 fn count_window_increases(nums: &Vec<i64>) -> usize {
-  let range = 0..nums.len() - 2;
-  let transformed: Vec<i64>  = range.map(|i|
-      nums[i] + nums[i+1] + nums[i+2]
-  ).collect::<Vec<i64>>();
-  count_increases(&transformed)
+    let range = 0..nums.len() - 2;
+    let transformed: Vec<i64> = range
+        .map(|i| nums[i] + nums[i + 1] + nums[i + 2])
+        .collect::<Vec<i64>>();
+    count_increases(&transformed)
 }
 
 fn day1() {
-  let nums = get_nums("data/nums.txt").unwrap();
-  let increases = count_increases(&nums);
-  assert!(increases == 1616);
-  let window_increases = count_window_increases(&nums);
-  assert!(window_increases == 1645);
+    let nums = get_nums("data/nums.txt").unwrap();
+    let increases = count_increases(&nums);
+    assert!(increases == 1616);
+    let window_increases = count_window_increases(&nums);
+    assert!(window_increases == 1645);
 }
 
 fn location_position(moves: &Vec<(String, i64)>) -> (i64, i64) {
-  moves.iter().fold(
-    (0, 0),
-    |(x_coord, y_coord), (direction, magnitude)|
-      match direction.as_str() {
-        "forward" => (x_coord + magnitude, y_coord),
-        "down" => (x_coord, y_coord + magnitude),
-        "up" => (x_coord, y_coord - magnitude),
-        _ => (x_coord, y_coord)
-      }  
-  )
+    moves.iter().fold(
+        (0, 0),
+        |(x_coord, y_coord), (direction, magnitude)| match direction.as_str() {
+            "forward" => (x_coord + magnitude, y_coord),
+            "down" => (x_coord, y_coord + magnitude),
+            "up" => (x_coord, y_coord - magnitude),
+            _ => (x_coord, y_coord),
+        },
+    )
 }
 
 fn aimed_location_position(moves: &Vec<(String, i64)>) -> (i64, i64, i64) {
-  moves.iter().fold(
-    (0, 0, 0),
-    |(x_coord, y_coord, aim), (direction, magnitude)|
-      match direction.as_str() {
-        "forward" => (x_coord + magnitude, y_coord + aim * magnitude, aim),
-        "down" => (x_coord, y_coord, aim + magnitude),
-        "up" => (x_coord, y_coord, aim - magnitude),
-        _ => (x_coord, y_coord, aim)
-      }  
-  )
+    moves.iter().fold(
+        (0, 0, 0),
+        |(x_coord, y_coord, aim), (direction, magnitude)| match direction.as_str() {
+            "forward" => (x_coord + magnitude, y_coord + aim * magnitude, aim),
+            "down" => (x_coord, y_coord, aim + magnitude),
+            "up" => (x_coord, y_coord, aim - magnitude),
+            _ => (x_coord, y_coord, aim),
+        },
+    )
 }
 
 fn day2() {
-  let directions = get_directions("data/directions.txt").unwrap();
-  let (x_coord, y_coord) = location_position(&directions);
-  println!("POS {} {} {}", x_coord, y_coord, x_coord * y_coord);
-  let (x_coord, y_coord, aim) = aimed_location_position(&directions);
-  println!("POS {} {} {} {}", x_coord, y_coord, aim, x_coord * y_coord);
-
+    let directions = get_directions("data/directions.txt").unwrap();
+    let (x_coord, y_coord) = location_position(&directions);
+    println!("POS {} {} {}", x_coord, y_coord, x_coord * y_coord);
+    let (x_coord, y_coord, aim) = aimed_location_position(&directions);
+    println!("POS {} {} {} {}", x_coord, y_coord, aim, x_coord * y_coord);
 }
 
 fn main() {
-  day1();
-  day2();
-  println!("on track!");
+    day1();
+    day2();
+    println!("on track!");
 }
--- util/mod.rs
+++ util/mod.rs
@@ -1,33 +1,36 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
+pub fn get_lines(filename: &str) -> io::Result<std::io::Lines<io::BufReader<File>>> {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 pub fn get_nums(filename: &str) -> io::Result<Vec<i64>> {
-  let mut nums: Vec<i64> = Vec::new();
-  let file = File::open(filename)?;
-  let lines = io::BufReader::new(file).lines();
-  for line in lines {
-    if let Ok(line) = line {
-      let num: i64 = line.parse().unwrap();
-      nums.push(num);
+    let mut nums: Vec<i64> = Vec::new();
+    let lines = get_lines(filename)?;
+    for line in lines {
+        if let Ok(line) = line {
+            let num: i64 = line.parse().unwrap();
+            nums.push(num);
+        }
     }
-  }
-  return Ok(nums);
+    return Ok(nums);
 }
 
 pub fn get_directions(filename: &str) -> io::Result<Vec<(String, i64)>> {
-  let mut res: Vec<(String, i64)> = Vec::new();
-  let file = File::open(filename)?;
-  let lines = io::BufReader::new(file).lines();
+    let mut res: Vec<(String, i64)> = Vec::new();
+    let lines = get_lines(filename)?;
 
-  for line in lines {
-    if let Ok(line) = line {
-      let split = line.split(" "); 
-      let vec: Vec<&str> = split.collect();
-      let direction = String::from(vec[0]);
-      let num: i64 = vec[1].parse().unwrap();
+    for line in lines {
+        if let Ok(line) = line {
+            let split = line.split(" ");
+            let vec: Vec<&str> = split.collect();
+            let direction = String::from(vec[0]);
+            let num: i64 = vec[1].parse().unwrap();
 
-      res.push((direction, num));
+            res.push((direction, num));
+        }
     }
-  }
-  return Ok(res);
+    return Ok(res);
 }
