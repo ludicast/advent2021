bf83355ecc3ef4ed70e62af585945f7497c4ac2c
Nate Kidwell <nate@ludicast.com>
2021-12-08 10:15:10 -0500

    update

--- file-reader/src/get_directions.rs
+++ file-reader/src/get_directions.rs
@@ -3,12 +3,15 @@ use util::read_lines;
 pub fn get_directions(filename: &str) -> Vec<(String, i64)> {
     let lines = read_lines(filename);
 
-    lines.iter().map(|line| {
-        let split = line.split(" ");
-        let vec: Vec<&str> = split.collect();
-        let direction = String::from(vec[0]);
-        let num: i64 = vec[1].parse().unwrap();
+    lines
+        .iter()
+        .map(|line| {
+            let split = line.split(" ");
+            let vec: Vec<&str> = split.collect();
+            let direction = String::from(vec[0]);
+            let num: i64 = vec[1].parse().unwrap();
 
-        (direction, num)
-    }).collect()
+            (direction, num)
+        })
+        .collect()
 }
--- file-reader/src/get_nums.rs
+++ file-reader/src/get_nums.rs
@@ -2,7 +2,5 @@ use util::read_lines;
 
 pub fn get_nums(filename: &str) -> Vec<i64> {
     let lines = read_lines(filename);
-    lines.iter().map(|line|
-        line.parse().unwrap()
-    ).collect()
+    lines.iter().map(|line| line.parse().unwrap()).collect()
 }
